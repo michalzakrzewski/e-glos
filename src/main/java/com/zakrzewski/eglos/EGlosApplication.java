1985e60ce756ae87b6b8f1411da3dc9ca1927911
Michał Zakrzewski <michal.zakrzewsky@gmail.com>
2025-09-30 22:21:18 +0200

    init commit

---
+++ src/main/java/com/zakrzewski/eglos/EGlosApplication.java
@@ -0,0 +1,13 @@
+package com.zakrzewski.eglos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGlosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EGlosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zakrzewski/eglos/EGlosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakrzewski.eglos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGlosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
